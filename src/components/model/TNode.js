eb76a3882b9f4dc0d0afb9704554d9025f49cf1f
areg-77 <aregak10@gmail.com>
2025-08-30 01:45:13 +0300

    added parent logic

--- src/components/model/TNode.js
+++ src/components/model/TNode.js
@@ -1,14 +1,26 @@
 let idCounter = 1;
 
 export class TNode {
+  get parent() { return this._parent; }
+  set parent(value) {
+    if (this._parent) {
+      const idx = this._parent.children.indexOf(this);
+      if (idx !== -1)
+        this._parent.children.splice(idx, 1);
+    }
+
+    this._parent = value;
+    this._parent.children.push(this);
+  }
+
   constructor(label, type, children = []) {
     this.id = idCounter++;
-    this.parent = null;
+    this._parent = null;
     this.label = label;
     this.type = type;
-    this.children = children;
+    this.children = [];
 
-    this.children.forEach(child => child.parent = this);
+    children.forEach(child => child.parent = this);
   }
 
   equals(other) {
